9ef68aaca4d303e5ae4fbee568df439ece7ba84e
Muspi Merol <wjj123345567@icloud.com>
2024-08-14 10:53:17 +0800

    chore: update model enums

--- src/providers/openai/index.ts
+++ src/providers/openai/index.ts
@@ -22,9 +22,8 @@ const providerOpenAI = () => {
         type: 'select',
         options: [
           { value: 'gpt-3.5-turbo-0125', label: 'gpt-3.5-turbo' },
-          { value: 'gpt-4-0125-preview', label: 'gpt-4-turbo-preview' },
           { value: 'gpt-4-turbo-2024-04-09', label: 'gpt-4-turbo' },
-          { value: 'gpt-4o-2024-05-13', label: 'gpt-4o' },
+          { value: 'gpt-4o-2024-08-06', label: 'gpt-4o' },
           { value: 'gpt-4o-mini-2024-07-18', label: 'gpt-4o-mini' },
         ],
         default: 'gpt-4o-mini-2024-07-18',
